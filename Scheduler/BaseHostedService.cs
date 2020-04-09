34e7a3de62f13f58159e2c757c2fc7ff3abe74d6
Simon Attila <attila.simon@lxline.hu>
2020-04-09 11:00:09 +0200

    .

--- Scheduler/BaseHostedService.cs
+++ Scheduler/BaseHostedService.cs
@@ -5,7 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 
-namespace LX.Common
+namespace Scheduler
 {
 	public class BaseHostedService : IHostedService, IDisposable
 	{
